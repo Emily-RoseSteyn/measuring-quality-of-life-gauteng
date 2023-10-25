f371d5dbea75d1aba0092f0d01eb58bd98ce4f7b
Emily-Rose Steyn <emilyrosesteyn@gmail.com>
2023-10-25 07:49:01 +0200

    Added module steps skeleton

---
+++ src/modules/preprocessing/preprocess.py
@@ -0,0 +1,9 @@
+import logging
+
+
+def main() -> None:
+    logging.info("In preprocessing")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/modules/training/train.py
@@ -0,0 +1,9 @@
+import logging
+
+
+def main() -> None:
+    logging.info("In training")
+
+
+if __name__ == "__main__":
+    main()
