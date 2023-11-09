5eb5943e8b8f134bc8ec4a7463d6efb4afbb8268
Emily-Rose Steyn <emilyrosesteyn@gmail.com>
2023-11-09 13:54:10 +0200

    Check planet api key in load env

--- src/utils/load_env.py
+++ src/utils/load_env.py
@@ -1,8 +1,13 @@
+import os
+
 from dotenv import load_dotenv
+from env_variable_keys import PLANET_API_KEY
 
 
 def main() -> None:
     load_dotenv()  # take environment variables from .env.
+    if PLANET_API_KEY in os.environ:
+        pass
 
 
 if __name__ == "__main__":
