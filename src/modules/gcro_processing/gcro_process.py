8f26251e8447a7a5136c7aed30f3bd81a8893359
Emily-Rose Steyn <emilyrosesteyn@gmail.com>
2024-01-21 20:40:32 +0200

    Add date info json

--- src/modules/gcro_processing/gcro_process.py
+++ src/modules/gcro_processing/gcro_process.py
@@ -1,5 +1,7 @@
+import json
 import logging
 import os
+from datetime import datetime
 
 import pandas as pd
 
@@ -16,11 +18,31 @@ def main() -> None:
     if not os.path.isdir(results_dir):
         os.makedirs(results_dir)
 
-    # 2021
+    # Write column headings and data types to json
     dtypes = df_gcro.dtypes
     dtypes.to_json(
         path_or_buf=f"{results_dir}/gcro-column-datatypes.json", default_handler=str
     )
+
+    # Write date information to json
+    date_format = "%Y-%m-%d"
+    total_interviews = len(df_gcro)
+    min_date = datetime.strftime(min(df_gcro["interview_date"]), date_format)
+    max_date = datetime.strftime(max(df_gcro["interview_date"]), date_format)
+    mean_date = datetime.strftime(df_gcro["interview_date"].mean(), date_format)
+
+    date_info = {
+        "total_interviews": total_interviews,
+        "min_date": min_date,
+        "max_date": max_date,
+        "mean_date": mean_date,
+    }
+
+    with open(f"{results_dir}/gcro-date-info.json", "w") as f:
+        json.dump(date_info, f, default=str)
+
+    # Ward Code Clusters
+    # TODO: Plot of distribution of interview dates
 
 
 if __name__ == "__main__":
