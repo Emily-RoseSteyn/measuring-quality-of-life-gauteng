f471a5a95b21991c4e911bc3a5185ca1de90e125
Emily-Rose Steyn <emilyrosesteyn@gmail.com>
2024-01-21 10:57:49 +0200

    Save gcro column data types to file

--- src/modules/gcro_processing/gcro_process.py
+++ src/modules/gcro_processing/gcro_process.py
@@ -1,4 +1,5 @@
 import logging
+import os
 
 import pandas as pd
 
@@ -8,7 +9,18 @@ def main() -> None:
     # Load gcro data
     gcro_dta = "./data/surveys/gcro-2021.dta"
     df_gcro = pd.read_stata(gcro_dta, convert_categoricals=False)
-    logging.info(df_gcro.head())
+
+    # TODO: Replace with step parameter
+    results_dir = "./outputs/processed-gcro"
+
+    if not os.path.isdir(results_dir):
+        os.makedirs(results_dir)
+
+    # 2021
+    dtypes = df_gcro.dtypes
+    dtypes.to_json(
+        path_or_buf=f"{results_dir}/gcro-column-datatypes.json", default_handler=str
+    )
 
 
 if __name__ == "__main__":
