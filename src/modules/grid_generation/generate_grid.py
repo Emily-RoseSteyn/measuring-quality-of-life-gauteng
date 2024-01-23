9d244b373f265914bf6fc4765dfd29612fede2cb
Emily-Rose Steyn <emilyrosesteyn@gmail.com>
2024-01-23 21:00:17 +0200

    Generating grid and images

--- src/modules/grid_generation/generate_grid.py
+++ src/modules/grid_generation/generate_grid.py
@@ -11,21 +11,18 @@ from plotnine import (
     labs,
     scale_fill_cmap,
 )
-from shapely import box
 
 
-def create_grid(bounds: np.ndarray, cell_size: float) -> GeoDataFrame:
+def create_grid(bounds: np.ndarray, cell_size: float, crs: str) -> GeoDataFrame:
     x_min, y_min, x_max, y_max = bounds
-    cols = np.arange(x_min, x_max + cell_size, cell_size)
-    rows = np.arange(y_min, y_max + cell_size, cell_size)
-    grid_cells = []
-    for x0 in cols:
-        for y0 in rows:
-            # bounds
-            x1 = x0 - cell_size
-            y1 = y0 + cell_size
-            grid_cells.append(box(x0, y0, x1, y1))
-    return gpd.GeoDataFrame(grid_cells, columns=["geometry"])
+    x_coords = list(np.arange(x_min, x_max, cell_size))
+    y_coords = list(np.arange(y_min, y_max, cell_size))
+
+    # Create all combinations of xy coordinates
+    coordinate_pairs = np.array(np.meshgrid(x_coords, y_coords)).T.reshape(-1, 2)
+    # Create a list of shapely points
+    geometries = gpd.points_from_xy(coordinate_pairs[:, 0], coordinate_pairs[:, 1])
+    return gpd.GeoDataFrame(geometries, columns=["geometry"], crs=crs)
 
 
 def main() -> None:
@@ -47,10 +44,7 @@ def main() -> None:
     # Creates grid of at specified resolution starting at the
     # min x and min y coordinates of the label bounding box
     # Rounded to 3 digit precision of the MOSAIKS grid
-    grid = create_grid(
-        bbox,
-        cell_size=target_resolution,
-    )
+    grid = create_grid(bbox, cell_size=target_resolution, crs=qol_labels.crs)
 
     # Plot grid over shp with values
     # noinspection PyTypeChecker
@@ -63,6 +57,23 @@ def main() -> None:
         + labs(x="", y="", fill="QoL Index")
     )
     fig.save(f"{results_dir}/grid-overlay.png", dpi=300)
+
+    # Spatial join grid over shapefile
+    joined_gdf = gpd.sjoin(grid, qol_labels, how="inner", predicate="within")
+
+    # Plot joined grid
+    # noinspection PyTypeChecker
+    #   - Warning is incorrect
+    fig = (
+        ggplot()
+        + geom_map(data=joined_gdf, mapping=aes(fill="qol_index"), stroke=0, size=1.2)
+        + scale_fill_cmap()
+        + labs(x="", y="", fill="QoL Index")
+    )
+    fig.save(f"{results_dir}/grid-gauteng-qol.png", dpi=300)
+
+    # Save CSV
+    joined_gdf.to_csv(f"{results_dir}/qol-labelled-grid.csv")
 
 
 if __name__ == "__main__":
