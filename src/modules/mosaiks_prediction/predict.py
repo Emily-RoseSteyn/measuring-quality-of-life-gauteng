0c427fa8ef686ca5ac7eb84226172f5bc7b0b3f4
Emily-Rose Steyn <emilyrosesteyn@gmail.com>
2024-01-24 07:55:27 +0200

    Formatting

--- src/modules/mosaiks_prediction/predict.py
+++ src/modules/mosaiks_prediction/predict.py
@@ -1,3 +1,4 @@
+import tensorflow as tf
 from pandas import merge, read_csv
 from utils.logger import get_logger
 
@@ -6,20 +7,25 @@ def main() -> None:
     logger = get_logger()
     logger.info("In prediction")
 
-    # load features (this may take a few mins)
+    # Tensorflow info logs
+    logger.debug("TensorFlow version: %s", tf.__version__)
+    device_name = tf.test.gpu_device_name()
+    if device_name != "/device:GPU:0":
+        logger.debug("GPU device not found - On for CPU time!")
+    else:
+        logger.debug("Found GPU at %s", device_name)
+
+    # Load features (this may take a few mins)
     features = read_csv("data/mosaiks-features/mosaiks_features.csv")
     features = features.sort_values(by=["Lat", "Lon"])
     logger.debug(features.head())
 
-    # load label data
+    # Load label data
     label_data = read_csv("outputs/grid/qol-labelled-grid.csv")
     # Magic number of 0.001 to remove error between this and the features
-    label_data["longitude"] = (
-        round(label_data["longitude"], 3) - 0.001
-    )  # 3 decimal places
-    label_data["latitude"] = (
-        round(label_data["latitude"], 3) - 0.001
-    )  # 3 decimal places
+    # TODO: Fix this error in original script
+    label_data["longitude"] = round(label_data["longitude"], 3) - 0.001
+    label_data["latitude"] = round(label_data["latitude"], 3) - 0.001
     label_data = label_data.sort_values(by=["latitude", "longitude"])
     logger.debug(label_data.head())
 
