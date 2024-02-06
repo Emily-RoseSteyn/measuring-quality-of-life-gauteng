65a5b0dc5e28c349411384c1866ddc6c93203236
Emily-Rose Steyn <emilyrosesteyn@gmail.com>
2024-02-06 16:43:32 +0200

    Modified predict to have reusable functions

--- src/modules/mosaiks_prediction/predict.py
+++ src/modules/mosaiks_prediction/predict.py
@@ -1,6 +1,6 @@
 import os
+from typing import Any
 
-import numpy as np
 import pandas as pd
 import tensorflow as tf
 from pandas import merge, read_csv
@@ -9,17 +9,22 @@ from plotnine import (
     geom_abline,
     geom_point,
     ggplot,
+    ggtitle,
     labs,
     scale_color_cmap,
 )
+from sklearn import metrics
 from sklearn.linear_model import Ridge
 from sklearn.model_selection import train_test_split
 from utils.logger import get_logger
 
+ModelResult = tuple[str, Any, Any, float | Any]
+
+logger = get_logger()
+
 
 def main() -> None:
-    # TODO: Potentially split this up more
-    logger = get_logger()
+    # TODO: SPLIT THIS UP
     logger.info("In prediction")
 
     # Tensorflow info logs
@@ -118,27 +123,46 @@ def main() -> None:
         axis=1,
     )
 
+    # RIDGE
+    ridge_result = ridge_regression(x_test, x_train, y_test, y_train)
+    plot_results(plotting_coords, plotting_coords_train, y_test, y_train, ridge_result)
+
+
+def ridge_regression(
+    x_test: dict, x_train: dict, y_test: dict, y_train: dict
+) -> ModelResult:
     # RIDGE REGRESSION HERE
     ridge_reg = Ridge(alpha=1)  # alpha is the hyperparameter equivalent to lambda
-
     # Train the model
     ridge_reg.fit(x_train, y_train)
-
     # Make predictions
     y_pred = ridge_reg.predict(x_test)
-
     # Compute R^2 from true and predicted values
-    sum_squared_errors = np.sum((y_pred - y_test) ** 2)
-    total_sum_squares = np.sum((y_test - np.mean(y_test)) ** 2)
-    r2 = 1 - sum_squared_errors / total_sum_squares
-
+    r2 = metrics.r2_score(y_test, y_pred)
     logger.info("r2: %s", r2)
+
+    # Make predictions on training set for plotting maps
+    y_pred_train = ridge_reg.predict(x_train)
+    y_pred_train[y_pred_train < 0] = 0
+
+    model_type = "ridge"
+    return model_type, y_pred, y_pred_train, r2
+
+
+def plot_results(
+    plotting_coords: dict,
+    plotting_coords_train: dict,
+    y_test: dict,
+    y_train: dict,
+    result: ModelResult,
+) -> None:
+    model_type, y_pred, y_pred_train, r2 = result
 
     # Plots
     results_dir = "./outputs/mosaiks-prediction"
+
     if not os.path.isdir(results_dir):
         os.makedirs(results_dir)
-
     map_plot = pd.DataFrame(
         {
             "longitude": plotting_coords["longitude"],
@@ -147,20 +171,15 @@ def main() -> None:
             "observed": y_test,
         }
     )
-
     # Scatterplot observed vs predicted
     fig = (
         ggplot(map_plot, aes(x=y_pred, y=y_test))
         + geom_point(alpha=0.5)
         + geom_abline(intercept=0, slope=1, size=0.8, alpha=0.3)
         + labs(x="Predicted", y="Observed")
+        + ggtitle(f"Predicted vs Observed: r2 = {round(r2, 4)}")
     )
-
-    fig.save(f"{results_dir}/observed-vs-predicted.png", dpi=300)
-
-    # Make predictions on training set for plotting maps
-    y_pred_train = ridge_reg.predict(x_train)
-    y_pred_train[y_pred_train < 0] = 0
+    fig.save(f"{results_dir}/{model_type}-observed-vs-predicted.png", dpi=300)
     map_plot_train = pd.DataFrame(
         {
             "longitude": plotting_coords_train["longitude"],
@@ -169,7 +188,6 @@ def main() -> None:
             "observed": y_train,
         }
     )
-
     # Plot observed
     # noinspection PyTypeChecker
     #   - Warning is incorrect
@@ -184,8 +202,7 @@ def main() -> None:
         + scale_color_cmap(cmap_name="Spectral")
         + labs(x="Longitude", y="Latitude", title="Observed", color="QoL")
     )
-    observed.save(f"{results_dir}/observed.png", dpi=300)
-
+    observed.save(f"{results_dir}/{model_type}-observed.png", dpi=300)
     # Plot predicted
     # noinspection PyTypeChecker
     #   - Warning is incorrect
@@ -200,7 +217,7 @@ def main() -> None:
         + scale_color_cmap(cmap_name="Spectral")
         + labs(x="Longitude", y="Latitude", title="Predicted", color="QoL")
     )
-    predicted.save(f"{results_dir}/predicted.png", dpi=300)
+    predicted.save(f"{results_dir}/{model_type}-predicted.png", dpi=300)
 
 
 if __name__ == "__main__":
