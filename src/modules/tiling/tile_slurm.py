0a6bab04edfa4853b8dd01ecb2913ecc309746cb
Emily-Rose Steyn <emilyrosesteyn@gmail.com>
2024-02-14 17:41:36 +0200

    Add index to tile image

--- src/modules/tiling/tile_slurm.py
+++ src/modules/tiling/tile_slurm.py
@@ -36,7 +36,7 @@ def tile_slurm(file_list: list, output_directory: str) -> None:
         )
 
         # Do stuff here!
-        tile_image(item, output_directory)
+        tile_image(item, output_directory, index)
 
     # End do stuff
 
