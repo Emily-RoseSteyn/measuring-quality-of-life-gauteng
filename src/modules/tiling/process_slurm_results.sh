45cad205a064aa0300cccdd7278662e6747ec08a
Emily-Rose Steyn <emilyrosesteyn@gmail.com>
2024-03-18 16:41:46 +0200

    Minor tweaks for tar

--- src/modules/tiling/process_slurm_results.sh
+++ src/modules/tiling/process_slurm_results.sh
@@ -3,7 +3,7 @@
 results_dir="${PWD}/outputs/tiles"
 
 # Decompress results files
-for file in "$results_dir"/result.*; do tar xzf "${file}" -C "$results_dir" && rm "${file}"; done
+for file in "$results_dir"/result.*; do tar -xzf "${file}" -C "$results_dir" && rm "${file}"; done
 echo "Uncompressed all files in output folder"
 
 # When all workers done and outputs have been merged, merge geojson results only on the one node
