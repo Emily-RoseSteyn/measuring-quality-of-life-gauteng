fd1b481711eb0dc0d1c231d3bca7ddb6b807916c
Emily-Rose Steyn <emilyrosesteyn@gmail.com>
2024-03-18 16:18:16 +0200

    Rather process slurm results outside of sbatch

---
+++ src/modules/tiling/process_slurm_results.sh
@@ -0,0 +1,10 @@
+# Run from root dir of project
+results_dir="${PWD}/outputs/tiles"
+
+# Decompress results files
+for file in "$results_dir"/result.*; do tar xzf "${file}" -C "$results_dir" && rm "${file}"; done
+echo "Uncompressed all files in output folder"
+
+# When all workers done and outputs have been merged, merge geojson results only on the one node
+python src/modules/tiling/merge_geojson "$results_dir"
+echo "Merged geojson files in output folder"
