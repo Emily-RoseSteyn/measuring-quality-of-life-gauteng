e1be56794b452cdf53b63e44ee5fd337331b5b8c
Emily-Rose Steyn <emilyrosesteyn@gmail.com>
2024-05-16 20:25:38 +0200

    Add 2018 data to gcro processing

--- src/modules/2_gcro_processing/gcro_process.py
+++ src/modules/2_gcro_processing/gcro_process.py
@@ -3,59 +3,55 @@ import os
 from datetime import datetime
 
 import pandas as pd
+
+from gcro_constants import GCRO_KEY_MAP
 from utils.logger import get_logger
 
 
-def main() -> None:
-    logger = get_logger()
-    logger.info("In gcro data processing")
+def process_gcro_survey_by_date(date: str):
     # Load gcro data
-    gcro_dta = "./data/surveys/gcro-2021.dta"
+    gcro_dta = f"./data/surveys/gcro-{date}.dta"
     df_gcro = pd.read_stata(gcro_dta, convert_categoricals=False)
-
     # TODO: Replace with step parameter
-    results_dir = "./outputs/processed-gcro"
-
+    results_dir = f"./outputs/processed-gcro/{date}"
     if not os.path.isdir(results_dir):
         os.makedirs(results_dir)
-
     # Write column headings and data types to json
     dtypes = df_gcro.dtypes
     dtypes.to_json(
         path_or_buf=f"{results_dir}/gcro-column-datatypes.json", default_handler=str
     )
-
     # Write date information to json
     date_format = "%Y-%m-%d"
     total_interviews = len(df_gcro)
-    min_date = datetime.strftime(min(df_gcro["interview_date"]), date_format)
-    max_date = datetime.strftime(max(df_gcro["interview_date"]), date_format)
-    mean_date = datetime.strftime(df_gcro["interview_date"].mean(), date_format)
-
+    date_key = GCRO_KEY_MAP[date]["date"]
+    min_date = datetime.strftime(min(df_gcro[date_key]), date_format)
+    max_date = datetime.strftime(max(df_gcro[date_key]), date_format)
+    mean_date = datetime.strftime(df_gcro[date_key].mean(), date_format)
     date_info = {
         "total_interviews": total_interviews,
         "min_date": min_date,
         "max_date": max_date,
         "mean_date": mean_date,
     }
-
     with open(f"{results_dir}/gcro-date-info.json", "w") as f:
         json.dump(date_info, f, default=str)
-
     # Ward Code Clusters
-    selected_data = df_gcro[
-        [
-            "ward_code",
-            "F1servic",
-            "F2soclas",
-            "F3govsat",
-            "F4lifsat",
-            "F5health",
-            "F6safety",
-            "F7partic",
-            "QoLIndex_Data_Driven",
-        ]
-    ].rename(
+    # TODO: select other questions possibly?
+
+    ward_code_key = GCRO_KEY_MAP[date]["ward_code"]
+    keys = [
+        ward_code_key,
+        "F1servic",
+        "F2soclas",
+        "F3govsat",
+        "F4lifsat",
+        "F5health",
+        "F6safety",
+        "F7partic",
+        "QoLIndex_Data_Driven",
+    ]
+    selected_data = df_gcro[keys].rename(
         columns={
             "F1servic": "services",
             "F2soclas": "socioeconomic_status",
@@ -67,14 +63,21 @@ def main() -> None:
             "QoLIndex_Data_Driven": "qol_index",
         }
     )
-    ward_code_group = selected_data.groupby(["ward_code"])
-
+    ward_code_group = selected_data.groupby([ward_code_key])
     ward_code_clusters = (
         ward_code_group.size().to_frame(name="counts").join(ward_code_group.mean())
     )
     ward_code_clusters.to_csv(f"{results_dir}/gcro-clustered-data.csv")
-
     # TODO: Plot of distribution of interview dates
+
+
+def main() -> None:
+    logger = get_logger()
+    logger.info("In gcro data processing")
+    process_gcro_survey_by_date("2021")
+    process_gcro_survey_by_date("2018")
+    # TODO: 2016 is an issue because it doesn't have the indexes??
+    # process_gcro_survey_by_date("2016")
 
 
 if __name__ == "__main__":
