c802b2b116fcc13e44d2684a1d3d0f89e7d4d04f
Emily-Rose Steyn <emilyrosesteyn@gmail.com>
2024-05-30 07:44:35 +0200

    Test data split randomly by ward

--- src/modules/7_test_data_split/test_data_split.py
+++ src/modules/7_test_data_split/test_data_split.py
@@ -9,7 +9,7 @@ from utils.logger import get_logger
 logger = get_logger()
 
 
-def split_data(df: pd.DataFrame) -> tuple:
+def ward_test_data_split(df: pd.DataFrame) -> tuple:
     """
     Accepts a Pandas DataFrame and splits it into training and test data. Returns DataFrames.
 
@@ -27,20 +27,28 @@ def split_data(df: pd.DataFrame) -> tuple:
     # TODO: Ensure splitting by ward + distribution of some kind??
     # TODO: Distribution of these datasets needs to be fair?
     #  See e2e notebooks with stratified shuffle split
-    train, test = train_test_split(df, test_size=0.2, random_state=1)  # split the data with a validation size o 20%
+    # TODO: Do we care about the year?
+    ward_numeric_df = df.drop(["tile", "geometry", "year"], axis=1)
+    ward_numeric_df = ward_numeric_df.groupby("ward_code").mean().reset_index()
+    ward_tile_df = df[["ward_code", "tile"]]
+    ward_tile_df = ward_tile_df.groupby("ward_code").count().rename(columns={"tile": "tile_count"}).reset_index()
+
+    wards = ward_numeric_df.merge(ward_tile_df, on="ward_code", how="inner")
+    train, test = train_test_split(wards, test_size=0.2, random_state=1)  # split the data with a validation size o 20%
 
     logger.info("Descriptive statistics of train:")
     logger.info(f"Shape: {train.shape}")
     logger.info(train.describe())
+    logger.info(train["ward_code"].head())
 
     logger.info("Descriptive statistics of test:")
     logger.info(f"Shape: {test.shape}")
     logger.info(test.describe())
-
-    df.loc[train.index, "split"] = "train"
-    df.loc[test.index, "split"] = "test"
-    df.to_csv("outputs/model/train-test-split.csv")
-
+    logger.info(test["ward_code"].head())
+    #
+    # df.loc[train.index, "split"] = "train"
+    # df.loc[test.index, "split"] = "test"
+    # df.to_csv("outputs/model/train-test-split.csv")
     return train, test
 
 
@@ -56,7 +64,7 @@ def main() -> None:
     dataset = gpd.read_file("outputs/matched/gauteng-qol-cluster-tiles.geojson")
 
     # Split data into 8_training, validation, test datasets
-    split_data(dataset)
+    ward_test_data_split(dataset)
 
 
 if __name__ == "__main__":
