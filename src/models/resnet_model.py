2473db1b5fc14a31aed9dcda7886929fb3cf2f55
Emily-Rose Steyn <emilyrosesteyn@gmail.com>
2024-08-19 22:10:24 +0200

    Add average pooling layer to base keras model

--- src/models/resnet_model.py
+++ src/models/resnet_model.py
@@ -1,7 +1,7 @@
 from dvc.api import params_show
 from keras import layers, Model
 from keras.applications import ResNet50V2
-from keras.layers import GlobalAveragePooling2D, Dense, BatchNormalization, Dropout
+from keras.layers import Dense, BatchNormalization, Dropout
 
 from models.base_model import BaseModel
 from models.model_types import ModelType
@@ -18,16 +18,13 @@ class ResnetModel(BaseModel):
         inputs = layers.Input(shape=(256, 256, 3))
 
         # Using ResNet50 architecture - freezing base model
-        base_model = ResNet50V2(
-            input_tensor=inputs, weights="imagenet", include_top=False
+        base_keras_model = ResNet50V2(
+            input_tensor=inputs, weights="imagenet", include_top=False, pooling="avg"
         )
-        base_model.trainable = False
+        base_keras_model.trainable = False
 
         # Get outputs of base model
-        x = base_model.output
-
-        # Add global average pooling layer
-        x = GlobalAveragePooling2D(name="avg_pool")(x)
+        x = base_keras_model.output
 
         # Add a fully connected layer
         fc_units = params["fc_units"]
